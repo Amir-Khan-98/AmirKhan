b2c2777dc2b38be822ac2e633cd61fbf6d9a7ac8
Amir-Khan-98 <32929035+Amir-Khan-98@users.noreply.github.com>
2018-08-24 18:01:35 +0100

    Genetic Algorithms
    Experimented with data using mutations and crossover.

---
+++ Genetic Algorithms/Lab11/src/Lab11.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Lab11 
+{
+	public static void main(String args[])
+	{
+		//Read in the weights
+		//Make sure you change the filename as appropriate!
+		//This will only work if "c:\temp\1000 Primes.txt" exists!
+		ArrayList<Double> w = CS2004.ReadNumberFile("C:\\Users\\Amir Khan\\Documents\\1000Primes.txt");
+		//Set the weights
+		ScalesChrome.SetWeights(w);
+		//Run 10 repeats
+		for(int i=0;i<10;++i)
+		{
+			//Reset the fitness count
+			ScalesChrome.ClearFC();
+			//The following parameters are not very good!
+			//These are the ones you should try and optimise!
+			int popsize = 10;
+			double mrate = 0.0015;
+			double crate = 0.75;
+			//You will not need to change the following
+			SimpleGeneticAlgorithm ga = new SimpleGeneticAlgorithm(popsize,10,10,mrate,crate);
+			//Run the GA for 10,000 function calls
+			double f = ga.RunSGA(10000,false).GetFitness();
+			//System.out.println(f);
+			//ScalesChrome.println();
+		}
+	}
+}
